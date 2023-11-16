b94e18cf65f14ca866b448c2c1936a8e38633a9c
Zhejun HE <republicho@foxmail.com>
2023-11-16 12:03:22 +0800

    Update Server.java

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,83 +1,90 @@
-//import model.EncryptedMessage;
-//
-//import java.io.IOException;
-//import java.io.ObjectInputStream;
-//import java.io.ObjectOutputStream;
-//import java.net.ServerSocket;
-//import java.net.Socket;
-//import java.util.List;
-//import java.util.concurrent.Callable;
-//import java.util.concurrent.ConcurrentHashMap;
-//import java.util.concurrent.ExecutorService;
-//import java.util.concurrent.Executors;
-//
-///**
-// * @Author Anthony HE, anthony.zj.he@outlook.com
-// * @Date 15/10/2023
-// * @Description: Multithreaded server
-// *
-// * 1. We used a fixed thread pool to limit the potential
-// * resource usage. Thirty threads should be plenty in this project.
-// *
-// */
-//public class Server {
-//
-//    final static int PORT = 7000;
-//    private ConcurrentHashMap<String, List<EncryptedMessage>> messageMap;
-//
-//    public Server(){
-//        messageMap = new ConcurrentHashMap<>();
-//    }
-//
-//    public static void main(String[] args){
-//
-//        // thread pool
-//        ExecutorService pool = Executors.newFixedThreadPool(30);
-//        try(ServerSocket server = new ServerSocket(PORT)){
-//            while(true){
-//                try{
-//                    Socket connection = server.accept();
-//                    Callable<Void> task = new MessageProcess(connection);
-//                }catch (IOException ex){}
-//
-//            }
-//
-//        }catch (IOException ex){
-//            // Exception handling for server-level issues
-//            System.err.println(ex);
-//        }
-//
-//    }
-//
-//    private static class MessageProcess implements Callable<Void>{
-//
-//        private Socket connection;
-//
-//        MessageProcess(Socket connection){
-//            this.connection = connection;
-//        }
-//        @Override
-//        public Void call() throws Exception {
-//            try{
-//
-//                // Output stream for sending data to the client.
-//                ObjectOutputStream outputStream = new ObjectOutputStream(connection.getOutputStream());
-//                // Input stream for receiving data from the client.
-//                ObjectInputStream inputStream = new ObjectInputStream(connection.getInputStream());
-//
-//
-//
-//            }catch (IOException e){
-//                System.err.println(e);
-//            }finally {
-//                try{
-//                    connection.close();
-//                }catch (IOException e){
-//                    // ignore
-//                }
-//
-//            }
-//            return null;
-//        }
-//    }
-//}
+import model.EncryptedMessage;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+* @Author Anthony HE, anthony.zj.he@outlook.com
+* @Date 15/10/2023
+* @Description: Multithreaded server
+*
+* 1. We used a fixed thread pool to limit the potential
+* resource usage. Thirty threads should be plenty in this project.
+* 2. The server manages the public keys.  
+* 3. The server implements the trust model. 
+*/
+public class Server {
+
+   final static int PORT = 7000;
+   private ConcurrentHashMap<String, List<EncryptedMessage>> messageMap;
+   private Map<String, String> userPublicKeyMap;
+   private Map<String, Map<String, String>> trustRelationships;
+   
+
+   public Server(){
+       messageMap = new ConcurrentHashMap<>();
+       userPublicKeyMap = new HashMap<>();
+       trustRelationships = new HashMap<>();
+       
+   }
+
+   public static void main(String[] args){
+
+       // thread pool
+       ExecutorService pool = Executors.newFixedThreadPool(30);
+       try(ServerSocket server = new ServerSocket(PORT)){
+           while(true){
+               try{
+                   Socket connection = server.accept();
+                   Callable<Void> task = new MessageProcess(connection);
+               }catch (IOException ex){}
+
+           }
+
+       }catch (IOException ex){
+           // Exception handling for server-level issues
+           System.err.println(ex);
+       }
+
+   }
+
+   private static class MessageProcess implements Callable<Void>{
+
+       private Socket connection;
+
+       MessageProcess(Socket connection){
+           this.connection = connection;
+       }
+       @Override
+       public Void call() throws Exception {
+           try{
+
+               // Output stream for sending data to the client.
+               ObjectOutputStream outputStream = new ObjectOutputStream(connection.getOutputStream());
+               // Input stream for receiving data from the client.
+               ObjectInputStream inputStream = new ObjectInputStream(connection.getInputStream());
+
+
+
+           }catch (IOException e){
+               System.err.println(e);
+           }finally {
+               try{
+                   connection.close();
+               }catch (IOException e){
+                   // ignore
+               }
+
+           }
+           return null;
+       }
+   }
+}
