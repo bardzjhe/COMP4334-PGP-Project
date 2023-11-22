b59d0026e58bccd61590f78d2c6c6edf74043a20
Melvin-King <79367726+Melvin-King@users.noreply.github.com>
2023-11-22 14:19:56 +0800

    Added PGPTester in PGP

--- src/main/java/auth/PGPTester.java
+++ src/main/java/auth/PGPTester.java
@@ -7,6 +7,7 @@ import model.EncryptedMessage;
 
 public class PGPTester {
 
+
     public static void main(String[] args) {
         // generate keypairs
         KeyPair senderKeyPair = generateKeyPair();
